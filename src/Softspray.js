089758b063f9c537923c27b0066f765746146480
Marimuthu <marimuthusivanandi@gmail.com>
2021-12-13 11:46:36 -0600

    full files are reuplode

---
+++ src/Softspray.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './SoftSpray.css';
+
+const SoftSpray = () =>{
+			return (
+			<div className="main-container">
+				<nav className="header-container">
+				 <h1><span className="letter-s">S</span><span className="letter-o">O</span><span className="letter-f">F</span>
+				 <span className="letter-t">T</span><span className="letter-s">S</span><span className="letter-p">P</span>
+				 <span className="letter-r">R</span><span className="letter-a">A</span><span className="letter-y">Y</span></h1>
+				 <ul className="ul">
+				 	<li><a href="https://www.reviewsxp.com/" target="blank">PRODUCTS</a></li>
+				 	<li><a href="https://www.jobboardfinder.com" target="blank">SERVICES</a></li>
+				 	<li><a href="https://www.thebalancecareers.com" target="blank">ABOUT</a></li>
+				 	<li><a href="https://www.linkedin.com/login" target="blank">JOBS</a></li>
+				 	<li><a href="#something" target="blank">CONTACT</a></li>
+				 	<li className="phone-no">+91 6383670514</li>
+				 	<span className="text-email">Email: <a href="https://www.softspray@gmail.com" target="blank">softspray@gmail.com</a></span>
+				 </ul>
+				 </nav>
+				 <div className="image-container"></div>
+				 <footer>
+				 	<p className="p1">  A website (also written as web site) is a collection of web pages and related content that is 
+				 	identified by a common domain name and published on at least one web server. ... Websites are typically 
+				 	dedicated to a particular topic or purpose, such as news, education, commerce, entertainment, or social 
+				 	networking.</p>
+				 	<p className="p2">  A network consists of two or more computers that are linked in order to share resources (such as 
+				 	printers and CDs), exchange files, or allow electronic communications. The computers on a network 
+				 	may be linked through cables, telephone lines, radio waves, satellites, or infrared light beams.</p>
+				 	<p className="p3">  A server is a computer that provides data to other computers. It may serve data to systems on a 
+				 	local area network (LAN) or a wide area network (WAN) over the Internet. Many types of servers exist, 
+				 	including web servers, mail servers, and file servers. Each type runs software specific to the purpose
+				    of the server.</p>
+				 </footer>
+			</div>);
+
+
+	}
+export default SoftSpray;
+
